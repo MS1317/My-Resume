dccae670d9b0d28b08d78abf20416ad9cacf9116
MS1317 <manpreetsingh7732@gmail.com>
2024-01-04 23:28:10 +0530

    added fonts

--- header.php
+++ header.php
@@ -10,5 +10,10 @@
     
     <!-- Bootstarp CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap" rel="stylesheet">
 </head>
 <body class="main-body">
