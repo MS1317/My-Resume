05db5326bb0ef46462a6a20482a50d91dc399c98
MS1317 <manpreetsingh7732@gmail.com>
2024-05-05 22:11:24 +0530

    sidebar updated

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,17 +1,17 @@
 function openNav() {
     document.getElementById("mySidenav").style.maxWidth="250px";
     document.getElementById("mySidenav").style.width="100%";
-    document.getElementById("hero-sec").style.marginRight="81px";
-    document.getElementById("sideProfileBtn").style.display="none";
     document.getElementById("backdrop-filtr").style.backgroundColor="rgba(30,30,40,.88)";
+    document.getElementById("sideProfileBtn").style.display="none";
     document.getElementById("content").style.left="-100px";
+    document.getElementById("hero-sec").style.marginRight="81px";
 }
 
 function closeNav(){
     document.getElementById("mySidenav").style.maxWidth= "0";
     document.getElementById("mySidenav").style.width="0";
-    document.getElementById("hero-sec").style.marginRight="0px"
     document.getElementById("sideProfileBtn").style.display="block";
     document.getElementById("backdrop-filtr").style.backgroundColor="unset";
     document.getElementById("content").style.left="0px";
+    document.getElementById("hero-sec").style.marginRight="0px"
 }
