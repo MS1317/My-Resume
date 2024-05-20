731e554fffcbc8393128e03abe129f7adc3597aa
MS1317 <manpreetsingh7732@gmail.com>
2024-05-20 23:50:41 +0530

    added mobile localhsot,updated css

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 // Define base URL or root directory based on the environment
-if ($_SERVER['SERVER_NAME'] === 'localhost') {
+if ($_SERVER['SERVER_NAME'] === 'localhost'|| $_SERVER['SERVER_NAME'] === '192.168.1.208') {
     define('BASE_PATH', '/mscv'); // Local environment
 } else {
     define('BASE_PATH', ''); // Production environment
