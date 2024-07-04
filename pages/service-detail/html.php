633e69dea19b94e9bb65e054a7eb2e5757b028da
MS1317 <manpreetsingh7732@gmail.com>
2024-07-04 23:50:48 +0530

    style changes

--- pages/service-detail/html.php
+++ pages/service-detail/html.php
@@ -10,8 +10,80 @@ require $_SERVER['DOCUMENT_ROOT'] . BASE_PATH . '/fetch_data.php';
         <div class="item-wrapper">
             <div class="list-item">
                 <span class="web-no">01</span>
-                <div class="list-item-title">Title</div>
+                <h3 class="list-item-title">Title</h3>
             </div>
+            <div class="web-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis enim placeat explicabo quaerat! Earum cumque, officia ullam minus nobis recusandae facere dolorem iure consectetur nostrum a voluptas voluptates aperiam quia.</div>
+            <div class="web-link">weblink</div>
+        </div>
+
+        <div class="item-wrapper">
+            <div class="list-item">
+                <span class="web-no">01</span>
+                <h3 class="list-item-title">Title</h3>
+            </div>
+            <div class="web-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis enim placeat explicabo quaerat! Earum cumque, officia ullam minus nobis recusandae facere dolorem iure consectetur nostrum a voluptas voluptates aperiam quia.</div>
+            <div class="web-link">weblink</div>
+        </div>
+
+        <div class="item-wrapper">
+            <div class="list-item">
+                <span class="web-no">01</span>
+                <h3 class="list-item-title">Title</h3>
+            </div>
+            <div class="web-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis enim placeat explicabo quaerat! Earum cumque, officia ullam minus nobis recusandae facere dolorem iure consectetur nostrum a voluptas voluptates aperiam quia.</div>
+            <div class="web-link">weblink</div>
+        </div>
+
+        <div class="item-wrapper">
+            <div class="list-item">
+                <span class="web-no">01</span>
+                <h3 class="list-item-title">Title</h3>
+            </div>
+            <div class="web-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis enim placeat explicabo quaerat! Earum cumque, officia ullam minus nobis recusandae facere dolorem iure consectetur nostrum a voluptas voluptates aperiam quia.</div>
+            <div class="web-link">weblink</div>
+        </div>
+
+        <div class="item-wrapper">
+            <div class="list-item">
+                <span class="web-no">01</span>
+                <h3 class="list-item-title">Title</h3>
+            </div>
+            <div class="web-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis enim placeat explicabo quaerat! Earum cumque, officia ullam minus nobis recusandae facere dolorem iure consectetur nostrum a voluptas voluptates aperiam quia.</div>
+            <div class="web-link">weblink</div>
+        </div>
+
+        <div class="item-wrapper">
+            <div class="list-item">
+                <span class="web-no">01</span>
+                <h3 class="list-item-title">Title</h3>
+            </div>
+            <div class="web-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis enim placeat explicabo quaerat! Earum cumque, officia ullam minus nobis recusandae facere dolorem iure consectetur nostrum a voluptas voluptates aperiam quia.</div>
+            <div class="web-link">weblink</div>
+        </div>
+
+        <div class="item-wrapper">
+            <div class="list-item">
+                <span class="web-no">01</span>
+                <h3 class="list-item-title">Title</h3>
+            </div>
+            <div class="web-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis enim placeat explicabo quaerat! Earum cumque, officia ullam minus nobis recusandae facere dolorem iure consectetur nostrum a voluptas voluptates aperiam quia.</div>
+            <div class="web-link">weblink</div>
+        </div>
+
+        <div class="item-wrapper">
+            <div class="list-item">
+                <span class="web-no">01</span>
+                <h3 class="list-item-title">Title</h3>
+            </div>
+            <div class="web-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis enim placeat explicabo quaerat! Earum cumque, officia ullam minus nobis recusandae facere dolorem iure consectetur nostrum a voluptas voluptates aperiam quia.</div>
+            <div class="web-link">weblink</div>
+        </div>
+        <div class="item-wrapper">
+            <div class="list-item">
+                <span class="web-no">01</span>
+                <h3 class="list-item-title">Title</h3>
+            </div>
+            <div class="web-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis enim placeat explicabo quaerat! Earum cumque, officia ullam minus nobis recusandae facere dolorem iure consectetur nostrum a voluptas voluptates aperiam quia.</div>
             <div class="web-link">weblink</div>
         </div>
     </div>
