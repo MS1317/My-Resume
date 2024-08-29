db5f37cd3d5f8184dd7915dee63c6b1c7ed11d21
MS1317 <manpreetsingh7732@gmail.com>
2024-08-29 06:27:27 +0530

    testing

--- timline.php
+++ timline.php
@@ -170,7 +170,7 @@
     <div class="timeline">
         <div class="timeline-item 1" data-text="FATHER OF THE TURKS">
             <div class="timeline__content"><img class="timeline__img" src="assets/timeline/photo_2024-08-04_18-20-32.jpg" />
-                <h2 class="timeline__content-title">1881</h2>
+                <h2 class="timeline__content-title">188</h2>
                 <p class="timeline__content-desc">He was born in 1881 (probably in the spring) in Salonica, then an Ottoman city, now inGreece. His father Ali Riza, a customs official turned lumber merchant, died when Mustafawas still a boy. His mother Zubeyde, adevout and strong-willed
                     woman, raised him and his sister.</p>
             </div>
