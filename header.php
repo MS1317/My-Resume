6e85b65814958c915b51a1c999e5c64396f396ff
MS1317 <manpreetsingh76732@gmail.com>
2025-02-08 15:24:04 +0530

    added preview image

--- header.php
+++ header.php
@@ -1,9 +1,13 @@
+<!-- <?php
+
+?> -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="Permissions-Policy" content="interest-cohort=()">
+    <meta property="og:image" content="<?php echo $_SERVER['DOCUMENT_ROOT'] . BASE_PATH . '/assets/preview-image.png'; ?>" />
     <title>MSCV</title>
 
     <!-- Custom CSS -->
