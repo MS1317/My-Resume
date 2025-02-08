238fe9be4c8e3657c929f6343cc65f4e3691df89
MS1317 <manpreetsingh76732@gmail.com>
2025-02-08 15:31:47 +0530

    checking preview image

--- header.php
+++ header.php
@@ -1,13 +1,16 @@
-<!-- <?php
-
-?> -->
+<?php
+// Ensure config.php is included either directly or via index.php
+if (!defined('BASE_PATH')) {
+    require('config.php');  // Adjust the path as needed based on your directory structure
+}
+?> 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="Permissions-Policy" content="interest-cohort=()">
-    <meta property="og:image" content="<?php echo $_SERVER['DOCUMENT_ROOT'] . BASE_PATH . '/assets/preview-image.png'; ?>" />
+    <meta property="og:image" content="<?php echo BASE_PATH . '/assets/preview-image.png'; ?>" />
     <title>MSCV</title>
 
     <!-- Custom CSS -->
